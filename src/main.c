ebec6bcfde9ccede9fa1b0dc2205103df1230223
vijayskk <vijayskk007@gmail.com>
2024-07-18 22:11:27 +0530

    Data extraction finished

--- src/main.c
+++ src/main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
-    printf("Project is on...\n");
+    FILE * fp = fopen("files/perfect.mp3","r");
+    if (fp == NULL)
+    {
+        printf("Cannot read file...\n");
+        exit(1);
+    }
+    char ch;
+    printf("Byte,Data\n");
+    int index = 0;
+    while (ch!=EOF)
+    {
+        ch=fgetc(fp);
+        printf("%d,%.2x\n",index,ch);
+        index++;
+    }
+    
     return 0;
 }
